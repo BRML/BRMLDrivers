e47fa06b0f9b8a7db0aa705029e96d3c57c2f03a
Sebastian Urban <surban@tum.de>
2016-05-31 18:30:12 +0200

    Added documentation templates

---
+++ docs/content/index.fsx
@@ -0,0 +1,24 @@
+(*** hide ***)
+
+(**
+Deep.Net
+========
+
+Deep learning library for F#. Provides symbolic model differentiation, automatic differentiation and compilation to CUDA GPUs. 
+It includes optimizers and model blocks used in deep learning.
+
+The Deep.Net library can be <a href="https://nuget.org/packages/DeepNet">installed from NuGet</a>.
+
+ 
+Contributing and copyright 
+--------------------------
+
+The project is hosted on [GitHub][gh] where you can [report issues][issues], fork the project and submit pull requests. 
+The library is available under the [Apache License 2.0][license], which allows modification and redistribution for both commercial and non-commercial purposes.  
+
+  [content]: https://github.com/DeepMLNet/DeepNet/tree/0.2/docs/content
+  [gh]: https://github.com/DeepMLNet/DeepNet
+  [issues]: https://github.com/DeepMLNet/DeepNet/issues
+  [readme]: https://github.com/DeepMLNet/DeepNet/blob/master/README.md
+  [license]: https://github.com/DeepMLNet/DeepNet/blob/master/LICENSE
+*)
