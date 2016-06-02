c6a79a41a11aa60b88bfda1788f7cef19b161c2a
Sebastian Urban <surban@tum.de>
2016-06-02 18:11:17 +0200

    docs build fixes

--- Publish-Docs.ps1
+++ Publish-Docs.ps1
@@ -1,19 +1,19 @@
-$ErrorActionPreference = "Stop"
-
 $doctemp = "$env:TEMP\Docs"
 if (Test-Path $doctemp) { rm -Recurse -Force $doctemp }
-mkdir $doctemp
+mkdir $doctemp | Out-Null
 pushd $doctemp
-git clone -b gh-pages https://github.com/BRML/BRMLDrivers.git . 2>&1
+git clone -b gh-pages https://github.com/BRML/BRMLDrivers.git . 2>$null
 rm -Recurse *
 popd
 
+$ErrorActionPreference = "Stop"
 .\Generate-Docs.ps1
+$ErrorActionPreference = "Continue"
 cp -Recurse docs\output\* $doctemp\
 
 pushd $doctemp
-git add --all . 2>&1
-git commit -m "automatic documentation generation" 2>&1
-git push 2>&1
+git add --all . 2>$null
+git commit -m "automatic documentation generation" 2>$null
+git push 2>$null
 popd
 
